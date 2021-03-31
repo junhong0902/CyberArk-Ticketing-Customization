6387602086fbf50f82f214db86d4e305500b11cf
jh.choo0902@gmail.com <61861375+junhong0902@users.noreply.github.com>
2021-04-01 01:49:58 +0800

    Update ticket.ps1

--- CyberArk-Ticketing/modules/ticket.ps1
+++ CyberArk-Ticketing/modules/ticket.ps1
@@ -1,24 +1,10 @@
 ### Logging
 $ErrorActionPreference="SilentlyContinue"
-#Stop-Transcript | out-null
-#$ErrorActionPreference = "Continue"
+###Stop-Transcript | out-null
+###$ErrorActionPreference = "Continue"
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $scriptPath = $scriptPath + "\ticketinglog.txt"
 Start-Transcript -path $scriptPath | out-null #Start-Transcript -path $scriptPath -append | out-null
-
-# Trim & convert input from base64 to UTF8
-try
-{
-    $TicketID = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($args[0]))
-    $APIURL = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($args[1]))
-    $cArkRequester = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($args[2]))
-    $obj = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($args[3]))
-}
-catch
-{
-    write-host "Mandatory parameter is missing.\n"
-}
-
 
 # Trim and Clean string
 function magic
@@ -28,8 +14,27 @@ function magic
         $inputstg
     )
 
-    # Return result
-    return ($inputstg.ToLower().Trim())
+    $inputstg = $inputstg.ToLower()
+    return ($inputstg.Trim())
+    
+}
+
+# Trim & convert input from base64 to UTF8
+try
+{
+    $TicketID = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($args[0]))
+    $APIURL = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($args[1]))
+    $cArkRequester = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($args[2]))
+    $obj = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($args[3]))
+
+    $TicketID = magic $TicketID
+    $APIURL = magic $APIURL
+    $cArkRequester = magic $cArkRequester
+    $obj = magic $obj
+}
+catch
+{
+    write-host "Mandatory parameter is missing.\n"
 }
 
 # Ignore SSL certificate
@@ -96,8 +101,7 @@ switch($strActionName)
     'CHG'
     {    
         $secrets = (Invoke-RestMethod -Method Get -Uri "$restURL" -ContentType application/json)
-            
-        if ((magic $obj -eq (magic $secrets.obj)) -and ((magic $secrets.approver) -ne (magic $cArkRequester)) -and (magic $TicketID -eq (magic $secrets.ticketid)) -and (magic $cArkRequester -eq (magic $secrets.requester)))
+        if (((magic $obj) -eq (magic $secrets.obj)) -and ((magic $secrets.approver) -ne (magic $cArkRequester)) -and ((magic $TicketID) -eq (magic $secrets.ticketid)) -and ((magic $cArkRequester) -eq (magic $secrets.requester)))
         {
 	        echo "VALID"
         }else
@@ -111,9 +115,9 @@ switch($strActionName)
         $secrets = (Invoke-RestMethod -Method Get -Uri "$restURLget" -ContentType application/json)
         if ($secrets.get_length() -eq 0)
         {
-            $json = @{'ticketid' =  $ticketID.tolower();'requester' = $cArkRequester.tolower();'obj' = $obj.tolower()}
+            $json = @{'ticketid' =  (magic $ticketID);'requester' = (magic $cArkRequester);'obj' = (magic $obj)}
             
-            Invoke-RestMethod -Method Post -Uri "$restURL" -Body $json
+            Invoke-RestMethod -Method Post -Uri "$restURL" -Body $json | out-null
             $secrets = (Invoke-RestMethod -Method Get -Uri "$restURLget" -ContentType application/json)
             if (((magic $obj) -eq (magic $secrets.obj))  -and ((magic $TicketID) -eq (magic $secrets.ticketid)) -and ((magic $cArkRequester) -eq (magic $secrets.requester)))
             {
@@ -125,7 +129,7 @@ switch($strActionName)
         }
         else
         {
-            if ((magic $obj -eq (magic $secrets.obj))  -and (magic $TicketID -eq (magic $secrets.ticketid)) -and (magic $cArkRequester -eq (magic $secrets.requester)))
+            if (((magic $obj) -eq (magic $secrets.obj))  -and ((magic $TicketID) -eq (magic $secrets.ticketid)) -and ((magic $cArkRequester) -eq (magic $secrets.requester)))
             {
 	            echo "VALID"
             }else
